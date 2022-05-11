6d46d2a28e80a6e98cf18b6e2cc62c3459b1dc41
Its-Alann <76508163+Its-Alann@users.noreply.github.com>
2022-05-10 22:47:59 -0400

    Added components to home page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,17 @@
 import Navbar from './components/Navbar'
+import Slider from './components/Slider'
+import Button from '@mui/material/Button';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <Slider/>
+      <Button variant="contained" size="medium">Select Drone</Button>
     </div>
   );
 }
+
 
 export default App;
